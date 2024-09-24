bd215eca8314f3556498cd1a2322d407b32346a0
Pedro Félix <pedrovitorsf@gmail.com>
2024-09-24 09:41:17 -0300

    feat: question 4 and 5

--- script.js
+++ script.js
@@ -49,18 +49,21 @@ for(let k = 1; k < indice; k++) {
 }
 
 // 4) Descubra a lógica e complete o próximo elemento:
-// a) 1, 3, 5, 7, ___
-// b) 2, 4, 8, 16, 32, 64, ____
-// c) 0, 1, 4, 9, 16, 25, 36, ____
-// d) 4, 16, 36, 64, ____
-// e) 1, 1, 2, 3, 5, 8, ____
-// f) 2,10, 12, 16, 17, 18, 19, ____
+// a) 1, 3, 5, 7, 9
+// b) 2, 4, 8, 16, 32, 64, 128
+// c) 0, 1, 4, 9, 16, 25, 36, 49
+// d) 4, 16, 36, 64, 100
+// e) 1, 1, 2, 3, 5, 8, 13
+// f) 2, 10, 12, 16, 17, 18, 19, 20
 
 
 // 5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. 
 // Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas 
 // vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para 
-// descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?  
+// descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
 
+//RESPOSTA:
+// Podemos ligar o interruptor 1 e esperar até que a temperatura da lâmpada aqueça, isso fará com que possamos ligar o interruptor 2 e ir até a uma sala checar qual lâmpada está "quente". 
+// Desta forma, podemos encontrar 2 lâmpadas com apenas esta estratégia.
 
 // NÃO SE ESQUEÇA DE INSERIR O LINK DO SEU REPOSITÓRIO NO GITHUB COM O CÓDIGO FONTE QUE VOCÊ DESENVOLVEU.
